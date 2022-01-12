17452452fe0fe7b706cc15fa5d4ed0c668916f6f
Stetson Done <scdone@gmail.com>
2022-01-12 09:32:58 -0700

    troubleshooting

--- server/index.js
+++ server/index.js
@@ -7,3 +7,9 @@ app.get('/', function(req,res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
 
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
